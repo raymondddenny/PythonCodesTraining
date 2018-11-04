0525d76456741f15b24293833985bdd284b84fca
raymondddenny <42437225+raymondddenny@users.noreply.github.com>
2018-11-05 01:50:04 +0700

    Update characterInput.py

--- Turn100/characterInput.py
+++ Turn100/characterInput.py
@@ -1,11 +1,7 @@
 # simple program to tell when the person is  gonna turn to 100 years old
-# import datetime function to know the current year,month,date,etc
 import datetime
-
-# assign datetime to variable now
 now = (datetime.datetime.now())
 
-# variable costruction
 name = input("Siapakah Namamu ? : ")
 age = int(input("Berapa Umurmu saat ini : "))
 
@@ -20,6 +16,3 @@ print("hello " + name+ ", you will turn to 100 years old in " + str(turn100))
 turn100 = (100-age) + now.year
 print("hello " + name.capitalize() +
       ", you will turn to 100 years old in " + str(turn100))
-
-# str.capitalize() for return a copy of string with its first character Capitalized and the rest lowercased
-# the codes maybe not that pythonic yet, feel free to correct it :)
